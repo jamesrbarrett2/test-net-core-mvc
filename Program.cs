a68133bc45ad1401655f06d826ed6eace4d3dd51
James R. Barrett <jamesrbarrett2@gmail.com>
2019-04-21 20:57:56 -0400

    Changed Readme and commented Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+/*  
+**************************************
+    This is a play project written in VS Code
+     For C# MVC .NET with Bootstrap
+     James Barret
+     4/21/2019
+*****************************************
+ */
+
 namespace test_net_core_mvc
 {
     public class Program
